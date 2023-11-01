f29f351976f6d811b3bb9daecc4345bd9789dc23
domeradzki <domeradzki.szymon@gmail.com>
2023-11-01 17:37:38 +0100

    supa starter

---
+++ src/lib/constants.js
@@ -0,0 +1,2 @@
+export const REACT_APP_SUPABASE_URL = "https://mgqwxjrduhaqmjjavswp.supabase.co";
+export const REACT_APP_SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ncXd4anJkdWhhcW1qamF2c3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3NjUwNjMsImV4cCI6MjAxNDM0MTA2M30.6GElTuc0Pf-dx4b-60UO6LbIvm6cfopAvjrkhdk06Dc";
